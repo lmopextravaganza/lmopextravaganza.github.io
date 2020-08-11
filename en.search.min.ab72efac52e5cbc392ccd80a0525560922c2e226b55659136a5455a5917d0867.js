6566d638e5eb82a211b8dc1e89a52f6923d17737
Zach Neuman <zach@xinu.io>
2020-08-10 22:41:59 -0500

    Finishing the Dwarven Temple and Proceeding to Wave Echo Cave

--- en.search.min.ab72efac52e5cbc392ccd80a0525560922c2e226b55659136a5455a5917d0867.js
+++ en.search.min.ab72efac52e5cbc392ccd80a0525560922c2e226b55659136a5455a5917d0867.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.d75ad9d644266d3bd2c4b61a996c7412e64f45bcd3e90b8d13deb60464bcac96.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.ae447c1ca9aecaa5c259c9a0ce0cbcafd1d165f465e7f0408d533bae95c8d17d.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
